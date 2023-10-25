0d41c36169ff3837632a3c21ceea719c657e011e
Hasan Semih Aktaş <hsemihaktas@gmail.com>
2023-10-25 11:47:54 +0300

    Version 1.2
    [ADD] Yeni projenin bilgileri json dosyasına girildi.
    [ADD] Projelerin siteleri olduğunda dair bilgi yeri jsonda açıldı ve carda uygun şekilde butonu eklendi.

--- src/components/projeCard.jsx
+++ src/components/projeCard.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 
-const ProjeCard = ({ proje }) => {
+const ProjeCard = ({ project }) => {
   return (
     <>
       <div className="w-full h-auto p-6 bg-white rounded-xl shadow-xl hover:shadow-2xl hover:scale-110 transition-all transform duration-500">
-        {proje.resimDosya && (
+        {project.imageFile && (
           <img
             className="w-full h-60 object-cover rounded-t-md"
-            src={require(`../images/${proje.resimDosya}`)}
-            alt={proje.baslik}
+            src={require(`../images/${project.imageFile}`)}
+            alt={project.baslik}
           />
         )}
 
         <div className="mt-4">
-          <h1 className="text-2xl font-bold text-gray-700">{proje.baslik}</h1>
-          <p className="text-sm mt-2 text-gray-700"> {proje.aciklama}</p>
-          {proje.siteLink && (
-            <div className="mt-4 mb-2 flex justify-between ">
-              <button className="text-lg block font-semibold py-2 px-6 text-white hover:bg-orange-600 bg-orange-400 rounded-lg shadow hover:shadow-md transition duration-300">
-                <a href={proje.siteLink}>Code...</a>
+          <h1 className="text-2xl font-bold text-gray-700">{project.title}</h1>
+          <p className="text-sm mt-2 text-gray-700"> {project.description}</p>
+          <div className="mt-4 mb-2 flex justify-between ">
+          {project.codeLink && (
+              <button className="text-sm lg:text-lg block font-semibold py-2 px-2 lg:px-6 text-white hover:bg-orange-600 bg-orange-400 rounded-lg shadow hover:shadow-md transition duration-300">
+                <a href={project.codeLink}>Code...</a>
               </button>
-            </div>
           )}
+          {project.projectSite && (
+              <button className="text-sm lg:text-lg block font-semibold py-2 px-2 lg:px-6 text-white hover:bg-orange-600 bg-orange-400 rounded-lg shadow hover:shadow-md transition duration-300">
+                <a href={project.projectSite}>Website...</a>
+              </button>
+          )}
+          </div>
         </div>
       </div>
     </>
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -2,19 +2,19 @@ import React, { useState, useEffect } from "react";
 import ProjeCard from "./projeCard";
 
 const Project = () => {
-  const [projeler, setProjeler] = useState([]);
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     // JSON dosyasını fetch edin
     fetch("/projects.json") // JSON dosyanızın yolu
       .then((response) => response.json())
-      .then((data) => setProjeler(data))
+      .then((data) => setProjects(data))
       .catch((error) => console.error("JSON verileri alınamadı:", error));
   }, []);
 
   return (
     <>
-      <div className="w-screen lg:w-full h-auto lg:h-screen lg:flex lg:flex-wrap lg:justify-center lg:items-center p-20 gap-5 bg-[#363636]">
+      <div className="w-screen lg:w-full h-auto lg:h-auto lg:flex lg:flex-wrap lg:justify-center lg:items-center p-20 gap-5 bg-[#363636]">
         <div className="w-full">
           <h1 className="text-4xl font-semibold mb-4 text-center text-white">
             Project List
@@ -22,8 +22,8 @@ const Project = () => {
         </div>
         <div className="w-full lg:w-2/3 flex flex-wrap justify-center items-center">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-10">
-              {projeler.map((proje, index) => (
-                <ProjeCard key={index} proje={proje} />
+              {projects.map((project, index) => (
+                <ProjeCard key={index} project={project} />
               ))}
             </div>
           </div>
