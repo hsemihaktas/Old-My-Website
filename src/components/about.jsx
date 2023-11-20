8c51e7372c061f9b1af6e3d8722facae428b7716
Hasan Semih Aktaş <hsemihaktas@gmail.com>
2023-11-20 23:34:41 +0300

    Version 1.4.2
    [FIXED] About divi düzeltildi ve Semih isminin rengi değiştirildi.
    [EDIT] Hakkımda yazısı güncellendi.

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,23 +6,28 @@ const About = () => {
   const handleDownload = () => {
     const anchor = document.createElement("a");
     anchor.href = "/cv.pdf";
-    anchor.download = "HasanSemihAktasCV.pdf"; 
+    anchor.download = "HasanSemihAktasCV.pdf";
     anchor.click();
   };
   return (
     <>
-      <div className="w-full lg:w-full h-auto lg:h-screen lg:flex items-center justify-center gap-5 bg-[#242424]">
-        <div className="text-white w-full py-10 px-5 lg:w-2/3 lg:px-60 ">
-          <div className="text-3xl my-3 lg:text-5xl lg:my-5">Who is Semih?</div>
+      <div className="w-full lg:w-full h-auto lg:min-h-screen lg:h-auto lg:flex items-center justify-center gap-5 bg-[#242424]">
+        <div className="text-white w-full py-10 px-5 lg:w-2/3 lg:px-20 ">
+          <div className="text-3xl my-3 lg:text-5xl lg:my-5">Who is <span className="text-[#df9100]">Semih</span>?</div>
           <div className="text-l my-3 lg:text-xl lg:my-5">
-            Hi, I'm Semih,
-            <br /> I entered the world of computer engineering in 2019 by
-            winning the Computer Engineering department of Sakarya University.
-            After studying at Sakarya University for 1 year, I transferred to
-            Istanbul Medeniyet University. I have seen various software
-            languages in my university life. Among these software languages, I
-            saw ReactJS and NodeJS as close to myself and I am trying to improve
-            myself in these areas.
+            I started my computer engineering career at Sakarya University,
+            Department of Computer Engineering in 2019. After my first year, I
+            transferred to Istanbul Medeniyet University to further increase my
+            knowledge and experience. During my university life, I was
+            introduced to various software languages, but I was particularly
+            interested in ReactJS and NodeJS. While I have deepened in creating
+            user interfaces and component development on ReactJS, I have
+            increased my server-side application development skills with NodeJS.
+            I have an approach that focuses on continuous self-improvement and I
+            successfully apply this knowledge in my projects by following the
+            innovations in the software world. My goal is to develop
+            user-friendly and effective software by making the most of the
+            opportunities offered by technology.
           </div>
           <div className=" w-full flex flex-wrap">
             <a
@@ -83,7 +88,11 @@ const About = () => {
         </div>
         <div className="w-0 lg:w-1/3 lg:h-1/2 lg:flex lg:flex-wrap ">
           <div className="w-full ">
-            <img src={QuestionPNG} alt="question" className="drop-shadow-[0_35px_35px_rgba(0,65,255,0.7)]  hover:scale-110 transition-all transform duration-500"/>
+            <img
+              src={QuestionPNG}
+              alt="question"
+              className="drop-shadow-[0_35px_35px_rgba(0,65,255,0.7)]  hover:scale-110 transition-all transform duration-500"
+            />
           </div>
         </div>
       </div>
